e2d0dc491c5bafde4569b6e0717e7881d8cbfe2a
wdDai <wendingdai@gmail.com>
2019-06-04 16:53:00 +1200

    First uncomp

---
+++ src/com/WendingTest/Main.java
@@ -0,0 +1,12 @@
+package com.WendingTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("www.trademe.com.nz");
+    }
+}
